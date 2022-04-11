d109e0216f1e2216d2cc749eeb50c5c7956aba9d
zsattari <famil.shia14@gmail.com>
2022-04-11 14:32:25 +0430

    core service

--- apps/core/mod.ts
+++ apps/core/mod.ts
@@ -1,107 +1,124 @@
 import {
-  ActFn,
-  getActs,
-  getDynamicActs,
-  getSchemas,
-  number,
-  object,
-  runServer,
-  selectStruct,
-  setAct,
-  string,
+    ActFn,
+    getActs,
+    getDynamicActs,
+    getSchemas,
+    number,
+    object,
+    runServer,
+    selectStruct,
+    setAct,
+    setService,
+    string,
 } from "/home/zahra/work/lesan/mod.ts";
-import {
-  createCoreCountrySchema,
-  createCoreUserSchema,
-} from "../../libs/dbs/schemas/core/mod.ts";
+import { createCoreCountrySchema, createCoreUserSchema } from "../../libs/dbs/schemas/core/mod.ts";
+// import { addWareTypeFn, addWareTypeValidator } from "../ecommerce/mod.ts";
 
 const country = createCoreCountrySchema();
 const user = createCoreUserSchema();
 
 const addUserValidator = () => {
-  return object({
-    set: object({
-      name: string(),
-      age: number(),
-    }),
-    get: selectStruct("user", { country: 1, nnno: 1 }),
-  });
+    return object({
+        set: object({
+            name: string(),
+            age: number(),
+        }),
+        get: selectStruct("user", { country: 1, nnno: 1 }),
+    });
 };
 const addUserFn: ActFn = (body) => {
-  const schemas = getSchemas();
-  const acts = getDynamicActs();
+    const schemas = getSchemas();
+    const acts = getDynamicActs();
 
-  /*
+    /*
   *  @LOG @DEBUG @INFO
   *  This log written by ::==> {{ syd }}
   *
   *  Please remove your log after debugging
   */
-  console.group("schemas ------ ");
-  console.log(" ============= ");
-  console.log();
-  console.info("schemas", JSON.stringify(schemas, null, 2));
-  console.log();
-  console.log(" ============= ");
-  console.groupEnd();
+    console.group("schemas ------ ");
+    console.log(" ============= ");
+    console.log();
+    console.info("schemas", JSON.stringify(schemas, null, 2));
+    console.log();
+    console.log(" ============= ");
+    console.groupEnd();
 
-  /*
+    /*
   *  @LOG @DEBUG @INFO
   *  This log written by ::==> {{ syd }}
   *
   *  Please remove your log after debugging
   */
-  console.group("acts ------ ");
-  console.log(" ============= ");
-  console.log();
-  console.info(JSON.stringify(acts, null, 2));
-  console.log();
-  console.log(" ============= ");
-  console.groupEnd();
-  return {
-    what: "what you said",
-  };
+    console.group("acts ------ ");
+    console.log(" ============= ");
+    console.log();
+    console.info(JSON.stringify(acts, null, 2));
+    console.log();
+    console.log(" ============= ");
+    console.groupEnd();
+    return {
+        what: "what you said",
+    };
 };
 setAct({
-  type: "dynamic",
-  schema: "user",
-  fn: addUserFn,
-  actName: "addUser",
-  validator: addUserValidator(),
+    type: "dynamic",
+    schema: "user",
+    fn: addUserFn,
+    actName: "addUser",
+    validator: addUserValidator(),
 });
 
 const addCounntryValidator = () => {
-  return object({
-    set: object({
-      name: string(),
-      age: number(),
-    }),
-    get: selectStruct("country", 2),
-  });
+    return object({
+        set: object({
+            name: string(),
+            age: number(),
+        }),
+        get: selectStruct("country", 2),
+    });
 };
 
 const addCountryfn: ActFn = (body) => {
-  /*
+    /*
   *  @LOG @DEBUG @INFO
   *  This log written by ::==> {{ syd }}
   *
   *  Please remove your log after debugging
   */
-  console.group("body ------ from addCountryfn");
-  console.log(" ============= ");
-  console.log();
-  console.info({ body }, " ------ ");
-  console.log();
-  console.log(" ============= ");
-  console.groupEnd();
+    console.group("body ------ from addCountryfn");
+    console.log(" ============= ");
+    console.log();
+    console.info({ body }, " ------ ");
+    console.log();
+    console.log(" ============= ");
+    console.groupEnd();
 };
 
 setAct({
-  type: "dynamic",
-  schema: "country",
-  fn: addCountryfn,
-  actName: "addCountry",
-  validator: addCounntryValidator(),
+    type: "dynamic",
+    schema: "country",
+    fn: addCountryfn,
+    actName: "addCountry",
+    validator: addCounntryValidator(),
 });
+
+////// set Service For ecommerce //////////
+////  url Base
+// setService("ecommerce", "http://localhost:8584/lesan");
+
+/// set Service
+//  Act Base
+// setService("ecommerce", {
+//    dynamic: {
+//        wareType: {
+//            addWareType: {
+//                validator: addWareTypeValidator(),
+//                fn: addWareTypeFn,
+//            },
+//        },
+//    },
+//    static: {},
+// });
 
 runServer({ port: 8585, playground: true });
