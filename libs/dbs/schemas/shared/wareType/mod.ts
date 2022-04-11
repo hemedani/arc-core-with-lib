f5ec7abc94a5a38b14e25c17d72790f39a6e66ee
zsattari <famil.shia14@gmail.com>
2022-04-11 08:13:42 +0430

    wareType in core

---
+++ libs/dbs/schemas/shared/wareType/mod.ts
@@ -0,0 +1,37 @@
+import {
+  addInrelations,
+  addOutRelations,
+  addPureModel,
+  InRelation,
+  optional,
+  OutRelation,
+  string,
+} from "/home/zahra/work/lesan/mod.ts";
+
+
+export const pureWareTypeObj = {
+name: string(),
+description: optional(string()),
+};
+
+
+
+export const wareTypeOutRel:Record<string,OutRelation> = {
+  wares: {
+    schemaName: "ware",    number: 50,
+    sort: { field: "_id", order: "desc" },
+  },
+};
+
+export const wareInRel = {};
+
+addPureModel("wareType", pureWareTypeObj);
+
+addOutRelations({
+  schemaName: "wareType",
+  outrelation: wareTypeOutRel,
+});
+
+
+
+
