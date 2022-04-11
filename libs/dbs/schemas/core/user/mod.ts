5338e157b2b2d579fea433742bfc44521c30d17f
zsattari <famil.shia14@gmail.com>
2022-04-11 14:32:54 +0430

    change import

--- libs/dbs/schemas/core/user/mod.ts
+++ libs/dbs/schemas/core/user/mod.ts
@@ -1,65 +1,62 @@
 import {
-  addInrelations,
-  addPureModel,
-  array,
-  boolean,
-  createStruct,
-  date,
-  enums,
-  getPureModel,
-  Infer,
-  InRelation,
-  number,
-  optional,
-  string,
-} from "/Users/syd/work/arc/lesan/mod.ts";
+    addInrelations,
+    addPureModel,
+    array,
+    boolean,
+    createStruct,
+    date,
+    enums,
+    getPureModel,
+    Infer,
+    InRelation,
+    number,
+    optional,
+    string,
+} from "/home/zahra/work/lesan/mod.ts";
 
 import db from "../../../db.ts";
-import {
-  pureUserObj as sharedPureUser,
-  userInRel as userInRelShared,
-} from "../../shared/mod.ts";
+import { pureUserObj as sharedPureUser, userInRel as userInRelShared } from "../../shared/mod.ts";
 
 export const createCoreUserSchema = () => {
-  const level = enums(["Admin", "Editor", "Author", "Ghost", "Normal"]);
-  const gender = enums(["Male", "Female"]);
+    const level = enums(["Admin", "Editor", "Author", "Ghost", "Normal"]);
+    const gender = enums(["Male", "Female"]);
 
-  const pureUserObj: Partial<typeof sharedPureUser> = {
-    name: string(),
-    age: number(),
-    lastName: string(),
-    phone: number(),
-    gender: gender,
-    birthDate: optional(date()),
-    postalCode: string(),
-    level: array(level),
-    email: optional(string()),
-    isActive: optional(boolean()),
-    creditCardNumber: optional(number()),
-  };
+    const pureUserObj: Partial<typeof sharedPureUser> = {
+        name: string(),
+        age: number(),
+        lastName: string(),
+        phone: number(),
+        gender: gender,
+        birthDate: optional(date()),
+        postalCode: string(),
+        level: array(level),
+        email: optional(string()),
+        isActive: optional(boolean()),
+        creditCardNumber: optional(number()),
+    };
 
-  const userInRel: Partial<typeof userInRelShared> = {
-    country: {
-      schemaName: "country",
-      type: "one",
-    },
-  };
-  const userOutRel = {};
+    const userInRel: Partial<typeof userInRelShared> = {
+        country: {
+            schemaName: "country",
+            type: "one",
+        },
+    };
+    const userOutRel = {};
 
-  addPureModel("user", pureUserObj);
+    addPureModel("user", pureUserObj);
 
-  addInrelations({
-    schemaName: "user",
-    inrelation: userInRel as Record<string, InRelation>,
-  });
+    addInrelations({
+        schemaName: "user",
+        inrelation: userInRel as Record<string, InRelation>,
+    });
 
-  //   it's comment due to create type when its need on app execution contenxt
-  // export type PureUser = Infer<typeof pureUser>;
+    //   it's comment due to create type when its need on app execution contenxt
+    // export type PureUser = Infer<typeof pureUser>;
 
-  const userStruct = createStruct("user");
+    const userStruct = createStruct("user");
 
-  //   it's comment due to create type when its need on app execution contenxt
-  type User = Infer<typeof userStruct>;
+    //   it's comment due to create type when its need on app execution contenxt
+    type User = Infer<typeof userStruct>;
 
-  return db.collection<User>("User");
+    return db.collection<User>("User");
 };
