1c3b0d1b98049b23fea96100dc2e8ebf20158460
zsattari <famil.shia14@gmail.com>
2022-04-13 15:18:48 +0430

    wareCategory

---
+++ libs/dbs/schemas/shared/wareCategory/mod.ts
@@ -0,0 +1,36 @@
+import {
+    addInrelations,
+    addOutRelations,
+    addPureModel,
+    InRelation,
+    optional,
+    OutRelation,
+    string,
+} from "/home/zahra/work/lesan/mod.ts";
+
+export const pureWareCategoryObj = {
+    name: string(),
+};
+
+export const wareCategoryOutRel: Record<string, OutRelation> = {
+    wares: {
+        schemaName: "ware",
+        sort: { field: "_id", order: "desc" },
+        number: 50,
+    },
+};
+export const wareCategoryInRel: Record<string, InRelation> = {
+    wareType: {
+        schemaName: "wareType",
+        type: "one",
+    },
+};
+
+addPureModel("wareCategory", wareCategoryPureObj);
+
+addOutRelations({
+    schemaName: "wareCategory",
+    outrelation: wareCategoryOutRel,
+});
+
+addInrelations({ schemaName: "wareCategory", inrelation: wareCategoryInRel });
